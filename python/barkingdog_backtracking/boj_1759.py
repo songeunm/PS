481df34748790bcbde9c00175ea133e1b4740870
문송은 <ansthddms123@gmail.com>
2025-01-11 14:37:00 +0900

    2025-01-11 boj bt (modify)

--- python/barkingdog_backtracking/boj_1759.py
+++ python/barkingdog_backtracking/boj_1759.py
@@ -12,7 +12,7 @@ lst = []
 def bt(apv: list, condition: list):
     # condition = [선택해야하는 전체 문자 수, 선택해야하는 남은 모음 수, 선택해야하는 남은 자음 수]
     if condition[0] <= 0:
-        if condition[1] == 0 and condition[2] == 0:
+        if condition[1] <= 0 and condition[2] <= 0:
             print("".join(lst))
         return
 
@@ -20,14 +20,12 @@ def bt(apv: list, condition: list):
         lst.append(apv[i])
         if apv[i] in "aeiou":
             # print(f"{apv[i]} - {lst} - {condition} 모음")
-            cond_1 = condition[1] - 1 if condition[1] >= 1 else 0
-            # print(f"    {[condition[0]-1, cond_1, condition[2]]}")
-            bt(apv[i+1:], [condition[0]-1, cond_1, condition[2]])
+            # print(f"    {[condition[0]-1, condition[1]-1, condition[2]}")
+            bt(apv[i+1:], [condition[0]-1, condition[1]-1, condition[2]])
         else:
             # print(f"{apv[i]} - {lst} - {condition} 자음")
-            cond_2 = condition[2] -1 if condition[2] >= 1 else 0
-            # print(f"    {[condition[0]-1, condition[1], cond_2]}")
-            bt(apv[i+1:], [condition[0]-1, condition[1], cond_2])
+            # print(f"    {[condition[0]-1, condition[1], condition[2]-1]}")
+            bt(apv[i+1:], [condition[0]-1, condition[1], condition[2]-1])
         lst.pop()
 
 
